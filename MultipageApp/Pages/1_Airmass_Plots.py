353ad0c4ebb86b2a9ec6c4bd8dbef5ffc813c2f8
gapaymykel8-blip <gapaymykel8@gmail.com>
2026-06-22 10:40:38 +0800

    Update 1_Airmass_Plots.py

--- MultipageApp/Pages/1_Airmass_Plots.py
+++ MultipageApp/Pages/1_Airmass_Plots.py
@@ -1,1 +1,40 @@
+import numpy as np ; import math
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import sys ; import os ; import glob
+import timeit
+from PIL import Image
+import datetime
+from timezonefinder import TimezoneFinder ; import pytz
+import streamlit as st
+
+import astropy
+import astropy.units as u
+import astropy.time as at
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.utils.data import get_pkg_data_filename
+import astropy.utils.data as aud
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation, get_body
+import astropy.coordinates as ac
+import astropy.constants as constants
+
+import astroquery.jplhorizons as jpl
+from astroquery.simbad import Simbad
+from astroquery.ipac.nexsci.nasa_exoplanet_archive import NasaExoplanetArchive as NEA
+from astroquery.vizier import Vizier
+
+from astroplan import FixedTarget, Observer
+import astroplan as ap
+import astroplan.plots as app
+from astroplan.plots import plot_sky
+
+import io
+
+import matplotlib.axes as maxes
+if not hasattr(maxes.Axes, 'plot_date'):
+    maxes.Axes.plot_date = lambda self, x, y, **kwargs: self.plot(x, y, **kwargs)
+
 
