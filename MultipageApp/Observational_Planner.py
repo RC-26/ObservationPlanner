4fb86936e5a4c820d0e85ecaa42deff106362ea0
gapaymykel8-blip <gapaymykel8@gmail.com>
2026-06-22 10:30:19 +0800

    Update Observational_Planner.py

--- MultipageApp/Observational_Planner.py
+++ MultipageApp/Observational_Planner.py
@@ -33,7 +33,7 @@ from astroplan.plots import plot_sky
 
 from ics import Calendar, Event
 
-from Utility import Render_Sidebar, Timezone_Finder, build_observational_data, Get_NEAdata, Generate_Transit_Dates, show_data_loc
+from Utility import Render_Sidebar, Timezone_Finder, build_observational_data, Get_NEAdata, Generate_Transit_Dates, show_data_loc, SN_obs, SN_long, SN_lat, SN_elev
 
 st.set_page_config(layout="wide")
 
