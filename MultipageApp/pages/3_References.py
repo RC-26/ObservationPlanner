baab138bd930e7ec664d730c043709c0383c66ba
RC-26 <82444487+RC-26@users.noreply.github.com>
2026-06-24 11:51:44 +0800

    Update 3_References.py

--- MultipageApp/pages/3_References.py
+++ MultipageApp/pages/3_References.py
@@ -59,4 +59,4 @@ st.divider   (width = "stretch")
 
 ############################################################################
 
-st.write ("This Streamlit app was developed by Sace, A.L.S.<sup>1</sup>, Gapay, D.M.V.<sup>2</sup>, Macalalad, E.P.<sup>1</sup>, & Reyes, R.C.<sup>1,3</sup>")
+st.write ("This Streamlit app was developed by Sace, A.L.S.<sup>1</sup>, Gapay, D.M.V.<sup>2</sup>, Macalalad, E.P.<sup>1</sup>, & Reyes, R.C.<sup>1,3</sup>", unsafe_allow_html = True)
