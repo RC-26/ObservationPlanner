4a340ecac0c45dd9c59a4beb2cfca18dc392f1a6
RC-26 <82444487+RC-26@users.noreply.github.com>
2026-06-24 17:17:42 +0800

    Add print statement for targets in Get_Transits
    Add print statement to display targets in Get_Transits function

--- MultipageApp/Utility.py
+++ MultipageApp/Utility.py
@@ -215,6 +215,7 @@ def Get_NEAdata(targets = None, Vband_limit = None):
 ####################################################################################################
 
 def Get_Transits(targets, start_date, end_date, obs_csv):
+    print (targets)
     if ',' in targets:
         targets = targets.split(', ')
     elif ',' not in targets and type(targets) != list:  
