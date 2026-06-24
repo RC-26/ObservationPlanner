e016183e5c9d9886e38222351089d0bdab36808c
RC-26 <82444487+RC-26@users.noreply.github.com>
2026-06-24 16:16:07 +0800

    Update Observational_Planner.py

--- MultipageApp/Observational_Planner.py
+++ MultipageApp/Observational_Planner.py
@@ -207,7 +207,7 @@ st.divider(width = 'stretch')
 ############################################################################
 
 st.subheader("Generate Transit Dates")
-st.caption("This simplifies the table above, giving the next Transit dates for the specified telescopes over the specified time")
+st.caption("Generates the dates of the predicted transit events of all exoplanets within the time window specified by the user.")
 
 options = [(tz, offset) for tz, offset in tz_offset.items()]
 
