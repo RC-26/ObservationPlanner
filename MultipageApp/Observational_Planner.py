04f62b56c6a0c2776fa0351e23e11a899268a9ed
gapaymykel8-blip <gapaymykel8@gmail.com>
2026-06-24 10:06:28 +0800

    Update Observational_Planner.py

--- MultipageApp/Observational_Planner.py
+++ MultipageApp/Observational_Planner.py
@@ -51,9 +51,9 @@ st.markdown("This pipeline is for helping users plan their observations by extra
 st.divider(width = "stretch")
 
 ############################################################################
-
+TDates = []
+TDates = st.session_state('TDates')
 st.subheader("Sky Net Observatories")
-
 
 ############################################################################
 
